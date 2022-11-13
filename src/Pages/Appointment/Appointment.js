1369ee23cdf541c79a77d797abafbc9e9ccaa4c9
maruf <dev.maruf112@gmail.com>
2022-11-13 22:56:54 +0600

    loading done

--- src/Pages/Appointment/Appointment.js
+++ src/Pages/Appointment/Appointment.js
@@ -1,12 +1,27 @@
 import { format } from "date-fns";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import AppointmentBanner from "../AppointmentBanner/AppointmentBanner";
 import AppointmentOptions from "../AvailableAppointment/AppointmentOptions";
 import AvailableAppointments from "../AvailableAppointment/AvailableAppointments";
 
 const Appointment = () => {
-  const [datePick, setDatePick] = useState(new Date());
+  const [loading, setLoading] = useState(false);
 
+  const [datePick, setDatePick] = useState(new Date());
+  const [selected, selectedDate] = useState(null);
+  useEffect(() => {
+    setLoading(true);
+    fetch("services.json")
+      .then((res) => res.json())
+      .then((data) => {
+        selectedDate(data);
+        setLoading(false);
+      });
+  }, []);
+  console.log(selected);
+  if (loading) {
+    return <p>I am loading now...</p>;
+  }
   return (
     <div>
       <AppointmentBanner
